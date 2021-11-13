fbbf6d85808ef5fa9de4e0440739f31f31e334e2
Mohammadhossein Sheikhsarraf <m.sheikhsarraf@gmail.com>
2021-11-13 22:27:00 +0100

    change file to get int number

--- src_to_implement/Optimization/Optimizers.py
+++ src_to_implement/Optimization/Optimizers.py
@@ -3,10 +3,8 @@ class Sgd:
     def __init__(self,learning_rate):
         #if not learning_rate.is_float():
         if not isinstance(learning_rate, float):
-            raise TypeError("learning rate should be float type")
-        else:
-            #if (learning_rate.is_integer()):
-                #raise TypeError("learning rate should be float type")
+            if not isinstance(learning_rate, int):
+                raise TypeError("learning rate must be set to an float or int")
             self.learning_rate = learning_rate
     def calculate_update(self,weight_tensor, gradient_tensor):
         updated_weights = np.subtract(weight_tensor, (np.multiply(self.learning_rate, gradient_tensor)))

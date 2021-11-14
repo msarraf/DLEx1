137d8eacf64acdde8807c09fa7efcdb53e23c6ed
Mohammadhossein Sheikhsarraf <m.sheikhsarraf@gmail.com>
2021-11-14 11:15:22 +0100

    correct optimizer file for getting learning rate

--- src_to_implement/Optimization/Optimizers.py
+++ src_to_implement/Optimization/Optimizers.py
@@ -5,7 +5,7 @@ class Sgd:
         if not isinstance(learning_rate, float):
             if not isinstance(learning_rate, int):
                 raise TypeError("learning rate must be set to an float or int")
-            self.learning_rate = learning_rate
+        self.learning_rate = learning_rate
     def calculate_update(self,weight_tensor, gradient_tensor):
         updated_weights = np.subtract(weight_tensor, (np.multiply(self.learning_rate, gradient_tensor)))
         return (updated_weights)
